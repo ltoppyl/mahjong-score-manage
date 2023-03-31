8151b968ead9a5c5520f71534fc435833d3a03fc
ltoppyl <toppyajibutu@gmail.com>
2023-03-31 23:53:32 +0900

    feat: FE の大枠の環境構築

---
+++ frontend/components/StorybookSample/index.stories.tsx
@@ -0,0 +1,11 @@
+import { StorybookSample } from "./index";
+
+export default {
+  component: StorybookSample,
+  title: "StorybookSample",
+  parameters: {},
+};
+
+const Template = () => <StorybookSample />;
+
+export const Default = Template.bind({});
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <p>Hello Next.js !</p>
+    </>
+  );
+};
+
+export default Home;

74e368f53b55297d9f3210ed8c3d32d4da18e33f
ltoppyl <toppyajibutu@gmail.com>
2023-04-01 15:32:20 +0900

    refactor: 不要な作業用コンソールの削除

--- frontend/components/StorybookSample/index.tsx
+++ frontend/components/StorybookSample/index.tsx
@@ -3,8 +3,6 @@ import React from "react";
 import { Button } from "@chakra-ui/react";
 
 export const StorybookSample = () => {
-  console.log("StorybookSample");
-
   return (
     <>
       <Button colorScheme="blue">Button</Button>

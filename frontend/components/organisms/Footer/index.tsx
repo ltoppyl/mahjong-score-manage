99eae790955fb23afc1e6bc4b882ede3065c855e
ltoppyl <toppyajibutu@gmail.com>
2023-04-08 20:47:53 +0900

    feat: フッターの作成

---
+++ frontend/components/organisms/Footer/index.tsx
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+import { Box, Button, Center } from "@chakra-ui/react";
+
+type Props = {
+  type: "home" | "result";
+};
+
+export const Footer = ({ type }: Props) => {
+  const router = useRouter();
+
+  return (
+    <Box h={24} w="100%">
+      <Center h="100%" alignItems="center">
+        <Button
+          bgColor="white"
+          borderColor="black"
+          color={type === "home" ? "#4A6F48" : "black"}
+          variant="outline"
+          onClick={() => {
+            if (type !== "home") {
+              router.push("/");
+            }
+          }}
+        >
+          追加画面へ
+        </Button>
+        {/* <HSpacer size={14} /> */}
+        <Button
+          bgColor="white"
+          borderColor="black"
+          color={type === "result" ? "#4A6F48" : "black"}
+          variant="outline"
+          onClick={() => {
+            if (type !== "result") {
+              router.push("/result");
+            }
+          }}
+        >
+          履歴画面へ
+        </Button>
+      </Center>
+    </Box>
+  );
+};

4e6716cab922d076ddc499da9bc49b96b0fc37bf
ltoppyl <toppyajibutu@gmail.com>
2023-04-09 11:32:45 +0900

    feat: フッターコンポーネントの作成

---
+++ frontend/components/organisms/Footer/index.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Footer } from "./index";
+
+const meta: Meta<typeof Footer> = {
+  title: "organisms/Footer",
+  component: Footer,
+};
+
+export default meta;
+type Story = StoryObj<typeof Footer>;
+
+export const Default: Story = {
+  args: {
+    type: "home",
+  },
+};

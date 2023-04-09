41d9d5414f35bb852d8f05083ca320909a3ed79f
ltoppyl <toppyajibutu@gmail.com>
2023-04-09 22:06:54 +0900

    chore: Recoil を使用できるような設定

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from "next/app";
-
+import { RecoilRoot } from "recoil";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <RecoilRoot>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </RecoilRoot>
   );
 }
 

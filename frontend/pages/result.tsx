64147f465a397ec50d6afad598e5a5272bf9cae3
ltoppyl <toppyajibutu@gmail.com>
2023-04-11 15:23:08 +0900

    feat: データを API を叩いて取得するように修正

--- frontend/pages/result.tsx
+++ frontend/pages/result.tsx
@@ -1,119 +1,23 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
 
+import { fetchRecordList } from "@/api/fetchRecordList";
 import { Result } from "@/components/templates/Result";
+import { loginState } from "@/stores/Recoil";
+import { FetchRecord } from "@/types/FetchRecord";
 
 const ResultPage: NextPage = () => {
-  const data = {
-    rankData: [
-      { name: "1", value: 10 },
-      { name: "2", value: 20 },
-      { name: "3", value: 30 },
-      { name: "4", value: 40 },
-    ],
-    resultDataList: [
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 12:00",
-        gameType: 4,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 13000,
-        point: -10.3,
-        date: "2023/4/1 13:00",
-        gameType: 3,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 14:00",
-        gameType: 4,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 15:00",
-        gameType: 4,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 15:00",
-        gameType: 4,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 15:00",
-        gameType: 4,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 15:00",
-        gameType: 4,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 15:00",
-        gameType: 4,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 15:00",
-        gameType: 4,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 15:00",
-        gameType: 4,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 15:00",
-        gameType: 4,
-      },
-      {
-        rule: "Mリーグルール",
-        rank: 2,
-        score: 25000,
-        point: 4.3,
-        date: "2023/4/1 15:00",
-        gameType: 4,
-      },
-    ],
-  };
-  return (
-    <>
-      <Result data={data} />
-    </>
-  );
+  const login = useRecoilValue(loginState);
+
+  const [data, setData] = useState<FetchRecord | undefined>(undefined);
+
+  useEffect(() => {
+    fetchRecordList(login.uid, setData);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <>{data && <Result data={data} />}</>;
 };
 
 export default ResultPage;

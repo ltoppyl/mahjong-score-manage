3df58c00cbeceb093736235e13d2b5e0defa07af
ltoppyl <toppyajibutu@gmail.com>
2023-04-11 12:42:18 +0900

    style: ページ全体への背景色を与える

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, Container } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider>
-        <Component {...pageProps} />
+        <Box bgColor="#4A6F48" minH={"100vh"}>
+          <Container maxW="container.md">
+            <Component {...pageProps} />
+          </Container>
+        </Box>
       </ChakraProvider>
     </RecoilRoot>
   );

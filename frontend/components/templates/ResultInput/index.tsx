e58c32cff620ef268a51180eaec2e37cb81153d8
ltoppyl <toppyajibutu@gmail.com>
2023-04-11 19:26:30 +0900

    style: 成績入力画面での style の修正

--- frontend/components/templates/ResultInput/index.tsx
+++ frontend/components/templates/ResultInput/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Center, HStack, Text } from "@chakra-ui/react";
+import { Center, HStack, Text, useBreakpointValue } from "@chakra-ui/react";
 
 import { LogoutButton } from "@/components/atoms/LogoutButton";
 import { VSpacer } from "@/components/atoms/Spacer";
@@ -11,14 +11,16 @@ type Props = {
   userName: string;
 };
 export const ResultInput = ({ userName }: Props) => {
+  const mediaType = useBreakpointValue({ base: "phone", md: "pc" });
+
   return (
     <div style={{ position: "relative", minHeight: "100vh" }}>
-      <VSpacer size={12} />
+      <VSpacer size={mediaType === "phone" ? 16 : 12} />
       <HStack justifyContent="space-between">
         <Text as="b">{userName}さんこんにちは</Text>
         <LogoutButton />
       </HStack>
-      <VSpacer size={4} />
+      <VSpacer size={mediaType === "phone" ? 24 : 4} />
       <Center>
         <ScoreInputField
           isFourMahjong={true}

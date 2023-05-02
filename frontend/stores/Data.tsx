c5bd904ff721cbcf470ea58841b4449a92505e16
ltoppyl <toppyajibutu@gmail.com>
2023-05-02 17:20:19 +0900

    feat: BE のデプロイ環境のリンクの追加

--- frontend/stores/Data.tsx
+++ frontend/stores/Data.tsx
@@ -1,4 +1,7 @@
-export const baseURL = "http://localhost:8000";
+export const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://mahjongbe-1-j0155660.deta.app"
+    : "http://localhost:8000";
 
 // ログインしているユーザ情報を保持するための key
 export const LOCAL_STORAGE_KEY = "userInfo";
